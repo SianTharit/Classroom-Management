214a1d818db148f62e528fc2799cc7afd4e83deb
SianTharit <Tharit.preecha1@gmail.com>
2026-06-11 09:50:04 +1000

    has no issues with indentation

--- src/pages/subjects/list.tsx
+++ src/pages/subjects/list.tsx
@@ -52,8 +52,11 @@ const SubjectList = () => {
                 accessorKey: 'description',
                 size: 300,
                 header: () => <p className="column-title">Description</p>,
-                cell: ({ getValue }) => <span className="truncate
-                line-clamp-2">{getValue<string>()}</span>,
+                cell: ({ getValue }) => (
+                    <span className="truncate line-clamp-2">
+                        {getValue<string>()}
+                    </span>
+                ),
             }
         ], []),
         refineCoreProps:{
